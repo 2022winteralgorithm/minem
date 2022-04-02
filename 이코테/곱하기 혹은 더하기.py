764202edd24d2a4fd014f66e4115900114e5a933
THEMATRIX1999 <96982019+THEMATRIX1999@users.noreply.github.com>
2022-04-02 21:13:20 +0900

    곱하기 혹은 더하기

---
+++ 이코테/곱하기 혹은 더하기.py
@@ -0,0 +1,12 @@
+num = input()
+
+result = int(num[0])
+
+for i in range(1, len(num)):
+    a = int(num[i])
+    if result <= 1 or a <= 1:
+        result += a
+    else:
+        result *= a
+
+print(result)
